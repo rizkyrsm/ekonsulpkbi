303b4627f25ccfb360c503fbf5c528ba069247a6
RSMTUF\RizkyrsmTUF <rizky.rsm@gmail.com>
2025-06-26 10:54:40 +0700

    no message

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,7 +10,23 @@ class Order extends Model
     use HasFactory;
     protected $primaryKey = 'id_order';
     protected $fillable = [
-        'id_order', 'id_user', 'id_konselor', 'nama_layanan', 
-        'voucher', 'total', 'payment_status', 'bukti_transfer'
+        'id_order',
+        'id_user',
+        'id_konselor',
+        'nama_layanan',
+        'voucher',
+        'total',
+        'payment_status',
+        'bukti_transfer'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
+
+    public function konselor()
+    {
+        return $this->belongsTo(User::class, 'id_konselor');
+    }
 }
